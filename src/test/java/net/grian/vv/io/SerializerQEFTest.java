07d281c1295e404358b4c2980a3f12f0a0230e71
Headaxe <janschultke@gmail.com>
2017-02-03 14:25:27 +0100

    updated plugin.yml

--- src/test/java/net/grian/vv/io/SerializerQEFTest.java
+++ src/test/java/net/grian/vv/io/SerializerQEFTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.*;
 
 public class SerializerQEFTest {
     
-    @Test
     public void makeBigQEF() throws Exception {
         Logger logger = VVTest.LOGGER;
         logger.setLevel(Level.FINE);
@@ -25,7 +24,7 @@ public class SerializerQEFTest {
             if ((x + y + z) % 2 == 0) array.setRGB(x, y, z, ColorMath.DEBUG1);
         }));
         
-        File out = new File(VVTest.DIR_FILES, "SerializerQEFTest.qef");
+        File out = new File(VVTest.DIR_FILES, "big.qef");
         if (!out.exists() && !out.createNewFile()) throw new IOException();
         
         new SerializerQEF(logger).toFile(array, out);
